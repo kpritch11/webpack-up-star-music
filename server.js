f2283d833559a3ae1add0c6c16ba49acabbd4705
Kurtis Pritchard <kpritch11@gmail.com>
2017-02-12 18:55:30 -0600

    Updated webpack middleware for production

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 const express = require('express');
-const webpackMiddleware = require('webpack-dev-middleware');
-const webpack = require('webpack');
-const webpackConfig = require('./webpack.config.js');
+const path = require('path');
 
 const app = express();
 
-app.use(webpackMiddleware(webpack(webpackConfig)));
+// Server routes above express configuration
+app.get('/hello', (request, response) => response.send({ hi: 'there' }));
 
-app.listen(3050, () => console.log('Listening on port 3050'));
+if (process.env.NODE_ENV == 'production') {
+    app.use(express.static('dist'));
+    app.get('*', (request, response) => {
+        response.sendFile(path.join(__dirname, 'dist/index.html'));
+    });
+} else {
+    const webpackMiddleware = require('webpack-dev-middleware');
+    const webpack = require('webpack');
+    const webpackConfig = require('./webpack.config.js');
+    app.use(webpackMiddleware(webpack(webpackConfig)));
+}
+
+const port = process.env.PORT || 3050;
+app.listen(port, () => console.log('Listening on port', port));
